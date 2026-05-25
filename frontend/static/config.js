d2072e9798957ee8c8df2c4271329db46093964b
kushal <dahalkushal2212@gmail.com>
2026-05-25 15:53:20 +0545

    add ?

--- frontend/static/config.js
+++ frontend/static/config.js
@@ -2,4 +2,4 @@ const API_BASE =
     window.location.hostname === "localhost" ||
     window.location.hostname === "127.0.0.1"
         ? "http://127.0.0.1:5000/"
-        : "https://cardamom-backend-00ks.onrender.com";
+        : "https://cardamom-backend-00ks.onrender.com/";
