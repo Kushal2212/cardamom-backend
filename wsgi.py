3931f04ce05ebd45b20a99c8d764dc932285f317
kushal <dahalkushal2212@gmail.com>
2026-05-25 14:19:40 +0545

    fix wsgi path

--- wsgi.py
+++ wsgi.py
@@ -1,7 +1,11 @@
 import sys
 import os
 
-sys.path.insert(0, os.path.join(os.path.dirname(__file__), "backend"))
+# Add backend folder to path
+backend_path = os.path.join(os.path.dirname(os.path.abspath(__file__)), "backend")
+sys.path.insert(0, backend_path)
+
+os.chdir(backend_path)
 
 from app import create_app
 
